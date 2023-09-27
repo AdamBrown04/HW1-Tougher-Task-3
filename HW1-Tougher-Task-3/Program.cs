4b5ac4bbbfc3b8aa3523798a0dee26cc4c44eeae
AdamBrown04 <adambrown2004@outlook.com>
2023-09-27 11:35:48 +0100

    asks user for their name and if they want to play the game, uses selection to see if input works correctly

--- HW1-Tougher-Task-3/Program.cs
+++ HW1-Tougher-Task-3/Program.cs
@@ -2,3 +2,19 @@
 //They start off with 3 health, a movement speed of 1, and a health regeneration speed of 0.1. All of these can be upgraded during play.
 //There are lots of different weapons in the game, but all can be represented the same way, with a name, a strength, and a durability.
 //The player is capable of holding a single weapon at a time. 
+
+Console.WriteLine("Welcome player! What is your name?");
+string userName = Console.ReadLine();
+
+Console.WriteLine($"Hello {userName}! Would you like to play this text based adventure game? Y/N");
+string playGame = Console.ReadLine();
+string playGameUpper = playGame.ToUpper();
+
+if (playGameUpper == "Y")
+{
+    Console.WriteLine("pass");
+}
+else
+{
+    Console.WriteLine("Fail");
+}
