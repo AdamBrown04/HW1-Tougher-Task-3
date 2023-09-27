574f47caf0deed8b3df5b1f8b94827e849b555bf
AdamBrown04 <adambrown2004@outlook.com>
2023-09-27 11:29:59 +0100

    Add project files.

---
+++ HW1-Tougher-Task-3/Program.cs
@@ -0,0 +1,4 @@
+﻿//Write classes and create example objects for the following game. At the start of the game the player gets to choose their name.
+//They start off with 3 health, a movement speed of 1, and a health regeneration speed of 0.1. All of these can be upgraded during play.
+//There are lots of different weapons in the game, but all can be represented the same way, with a name, a strength, and a durability.
+//The player is capable of holding a single weapon at a time. 
