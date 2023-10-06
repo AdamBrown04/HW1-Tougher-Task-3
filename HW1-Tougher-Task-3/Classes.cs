333bf3e4d712591fdeb7cb345364a8b9dad63767
AdamBrown04 <adambrown2004@outlook.com>
2023-10-06 10:44:23 +0100

    created a new file which is just for the classes so it's seperate from the main program

---
+++ HW1-Tougher-Task-3/Classes.cs
@@ -0,0 +1,24 @@
+﻿class Character
+{
+    public string characterName = ""; //name of the character, only used for dialogue
+    public string weaponRarityType = ""; //This is used to show that stats of weapons
+    public bool holdingWeapon = false; //sees if the player is currently holding a weapon
+    public float characterHealth = 5.0f; //how much health the character has 
+    public float characterHealthRegeneration = 0.1f; //how much health the character regenerates each turn
+    public int money = 500; //amount of in-program currency the character has
+    public int movementSpeed = 3; //how fast the character moves
+    public int characterStrength = 3; //affects the melee damage of a character
+                                      //e.g. if a weapon has 5 damage the strength would be added on top making it 8.
+}
+
+class Weapon
+{
+    public string weaponName = ""; //no weapon names implemented yet (could use to distinguish types of weapon)
+    public int weaponStrength; //how much damage the weapon does by itself
+    public int weaponDurability; //how many hits the weapon can do before it breaks (may add a repair feature)
+}
+
+class Enemy : Character
+{
+    public string enemyDifficulty = ""; //the level of difficulty of the enemy 
+}
--- HW1-Tougher-Task-3/Program.cs
+++ HW1-Tougher-Task-3/Program.cs
@@ -256,30 +256,9 @@ void showStats() //this is a function that will show the user the players and we
 }
 
 
-class Character
-{
-    public string characterName = ""; //name of the character, only used for dialogue
-    public string weaponRarityType = ""; //This is used to show that stats of weapons
-    public bool holdingWeapon = false; //sees if the player is currently holding a weapon
-    public float characterHealth = 5.0f; //how much health the character has 
-    public float characterHealthRegeneration = 0.1f; //how much health the character regenerates each turn
-    public int money = 500; //amount of in-program currency the character has
-    public int movementSpeed = 3; //how fast the character moves
-    public int characterStrength = 3; //affects the melee damage of a character
-                                      //e.g. if a weapon has 5 damage the strength would be added on top making it 8.
-} 
 
-class Weapon
-{
-    public string weaponName; //no weapon names implemented yet (could use to distinguish types of weapon)
-    public int weaponStrength; //how much damage the weapon does by itself
-    public int weaponDurability; //how many hits the weapon can do before it breaks (may add a repair feature)
-}
 
-class Enemy : Character
-{
 
-}
-
-//Console.Clear(); use to delete all text on screen ref: asked Oliver CW
+//references for code that I have taken from others
+//Console.Clear(); use to delete all text on screen ref: asked Oliver CW (another member of the class)
 //Task.Delay().Wait(); use to make program pause ref: stackoverflow.com/questions/5449956/how-to-add-a-delay-for-a-2-or-3-seconds
